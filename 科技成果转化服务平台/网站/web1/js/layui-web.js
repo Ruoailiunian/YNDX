3e0bc7c95458723e301f8dd98f65d9de7db75dbc
Ruoailiunian <47737419+Ruoailiunian@users.noreply.github.com>
2019-02-25 17:30:06 +0800

    2019-02-25下班

---
+++ 科技成果转化服务平台/网站/web1/js/layui-web.js
@@ -0,0 +1,16 @@
+
+//由于模块都一次性加载，因此不用执行 layui.use() 来加载对应模块，直接使用即可：
+;!function(){
+  var layer = layui.layer
+ // layer.msg('Hello World');
+  //layer.msg('Hello World');
+
+
+  //日期
+  var laydate = layui.laydate;
+    //执行一个laydate实例
+  laydate.render({
+    elem: '#Sr_date' //指定元素
+  });
+
+}();
